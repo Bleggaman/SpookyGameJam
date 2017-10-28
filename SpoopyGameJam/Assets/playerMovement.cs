e73c5f825056aed04441964c683f880b5128db35
ejordan1 <ejordan1@uw.edu>
2017-10-27 20:53:46 -0700

    initialPush

---
+++ SpoopyGameJam/Assets/playerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	public float moveSpeed;
+	public float crouchSpeed;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 velocityVector = Vector3.zero;
+		if (Input.GetKey(KeyCode.W)){
+			velocityVector.z += moveSpeed;
+		}
+		if (Input.GetKey(KeyCode.A)){
+			velocityVector.x -= moveSpeed;
+		}
+		if (Input.GetKey(KeyCode.S)){
+			velocityVector.z -= moveSpeed;
+		}
+		if (Input.GetKey(KeyCode.D)){
+			velocityVector.x += moveSpeed;
+		}
+		rb.velocity = velocityVector;
+	}
+}
